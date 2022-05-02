9700aa70d3ba3ef7484b578834b248e5ce13759f
Trevor Martin <tjm27@scasd.org>
2022-05-02 15:38:06 -0400

    Added rotationally-aware equals method as well as a rotate method

--- src/Polyomino.java
+++ src/Polyomino.java
@@ -10,14 +10,61 @@ public class Polyomino {
 		this.cells = new boolean[height][width];
 	}
 	
+	public Polyomino rotate() {
+		Polyomino res = new Polyomino(this.height, this.width);
+
+		for(int y = 0; y < this.height; y++) {
+			for(int x = 0; x < this.width; x++) {
+				int nx = y,
+					ny = this.width - 1 - x;
+				res.setCell(nx, ny, this.getCell(x, y));
+			}
+		}
+
+		return res;
+	}
+
+	public boolean shallowEquals(Polyomino other) {
+		if(this.width != other.width || this.height != other.height) {
+			return false;
+		}
+
+		for(int y = 0; y < this.height; y++) {
+			for(int x = 0; x < this.width; x++) {
+				if(this.getCell(x, y) != other.getCell(x, y)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
 	@Override
 	public boolean equals(Object other) {
 		if(other instanceof Polyomino) {
-			// TODO: Add equals. Two rotated polyominos must still be equal
+			// This is extremely inefficient, but it shouldn't be called often
+			if(this.shallowEquals((Polyomino)other)) {
+				return true;
+			}
+
+			Polyomino rot = this.rotate();
+			for(int i = 1; i < 4; i++) {
+				if(rot.shallowEquals((Polyomino)other)) {
+					return true;
+				}
+				if(i < 3) {
+					rot = rot.rotate();
+				}
+			}
 		}
 		return false;
 	}
 	
+	public boolean getCell(int x, int y) {
+		return this.cells[y][x];
+	}
+
 	public void setCell(int x, int y, boolean v) {
 		this.cells[y][x] = v;
 	}
