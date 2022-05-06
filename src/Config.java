b8683a6c11447bbb2482313455e23f27705c179a
Trevor Martin <tjm27@scasd.org>
2022-05-06 15:07:34 -0400

    Handling polyomino generation in Config; doesn't make sense, but this way its accessible

--- src/Config.java
+++ src/Config.java
@@ -1,4 +1,30 @@
+import java.util.LinkedList;
 
 public class Config {
-	public int polyomino_size = 4;
+	private PolyominoFactory factory;
+
+	private int polyomino_size = -1;
+	private LinkedList<Polyomino> polyominos;
+
+	public Config() {
+		this.factory = new PolyominoFactory();
+
+		this.set_polyomino_size(4);
+	}
+
+	public void set_polyomino_size(int size) {
+		if(size != this.polyomino_size) {
+			this.polyomino_size = size;
+
+			this.polyominos = this.factory.generatePolyominos(size);
+		}
+	}
+
+	public int get_polyomino_size() {
+		return this.polyomino_size;
+	}
+
+	public LinkedList<Polyomino> get_polyominos() {
+		return this.polyominos;
+	}
 }
