865082959d0b15c3cc650b5c4e33f2381b7410f3
Trevor Martin <tjm27@scasd.org>
2022-05-06 14:31:01 -0400

    Moved board drawing to a seperate class

---
+++ src/PlayCanvas.java
@@ -0,0 +1,66 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class PlayCanvas extends JPanel {
+	private final int CELL_SIZE = 16,
+		CELL_SPACING = 1,
+		BOARD_HEIGHT = 20,
+		BOARD_WIDTH = 10;
+	
+	private int[][] board;
+	
+	public PlayCanvas() {
+		// Board is by default all 0s
+		this.board = new int[BOARD_HEIGHT][BOARD_WIDTH];
+	}
+	
+	public void set_cell(int x, int y, int value) {
+		this.board[y][x] = value;
+	}
+	
+	public int get_cell(int x, int y) {
+		return this.board[y][x];
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		final Color[] colors = {
+			Color.WHITE,
+			Color.RED,
+			Color.ORANGE,
+			Color.GREEN,
+			Color.PINK
+		};
+		
+		for(int y = 0; y < BOARD_HEIGHT + 2; y++) {
+			int ty = y * (CELL_SIZE + CELL_SPACING);
+			for(int x = 0; x < BOARD_WIDTH + 2; x++) {
+				int tx = x * (CELL_SIZE + CELL_SPACING);
+				
+				if((x == 0) ||
+				   (y == 0) ||
+				   (x >= BOARD_WIDTH + 1) ||
+				   (y >= BOARD_HEIGHT + 1)) {
+					g.setColor(Color.GRAY);
+					
+					g.fill3DRect(tx, ty,
+						CELL_SIZE, CELL_SIZE,
+						true
+					);
+				} else {
+					int curr = this.board[y - 1][x - 1];
+					
+					g.setColor(colors[curr]);
+					
+					g.fill3DRect(tx, ty,
+						CELL_SIZE, CELL_SIZE,
+						curr != 0
+					);
+				}
+			}
+		}
+	}
+}
--- src/PlayPanel.java
+++ src/PlayPanel.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -7,49 +6,18 @@ public class PlayPanel extends JPanel {
 	private Config config;
 	private Navigator navigator;
 	
-	private int[][] board;
-	
-	private final int CELL_SIZE = 16, SPACING = 1;
+	private PlayCanvas canvas;
 	
 	public PlayPanel(Navigator nav, Config config) {
 		this.config = config;
 		this.navigator = nav;
 		
-		this.board = new int[20 + 2][10 + 2];
-		for(int i = 0; i < 20+2; i++) {
-			this.board[i][0] = 1;
-			this.board[i][10 + 1] = 1;
-		}
-		
-		for(int i = 1; i < 10+1; i++) {
-			this.board[0][i] = 1;
-			this.board[20 + 1][i] = 1;
-		}
+		this.canvas = new PlayCanvas();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		final Color[] colors = {
-			Color.WHITE,
-			Color.GRAY,
-			Color.RED,
-			Color.ORANGE,
-			Color.GREEN,
-			Color.PINK
-		};
-		
-		for(int y = 0; y < this.board.length; y++) {
-			for(int x = 0; x < this.board[y].length; x++) {
-				int curr = this.board[y][x];
-				
-				g.setColor(colors[curr]);
-				
-				g.fill3DRect(
-					x * (CELL_SIZE + SPACING),
-					y * (CELL_SIZE + SPACING),
-					CELL_SIZE, CELL_SIZE, curr != 0);
-			}
-		}
+		this.canvas.paintComponent(g);
 	}
 }
